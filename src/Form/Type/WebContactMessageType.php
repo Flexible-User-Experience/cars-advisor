3b2b5106334d6d7b0b7ddd4f9ec873245184ac78
David Romaní <david@flux.cat>
2020-12-30 12:55:10 +0100

    improve homapage composition - 4

--- src/Form/Type/WebContactMessageType.php
+++ src/Form/Type/WebContactMessageType.php
@@ -75,6 +75,9 @@ class WebContactMessageType extends AbstractType
                     'required' => true,
                     'mapped' => false,
                     'label' => 'Acepto las condiciones legales',
+                    'attr' => [
+                        'class' => 'custom-control-input',
+                    ],
                 ]
             )
             ->add(
