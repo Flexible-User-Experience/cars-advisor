b5c1f5bdfb2284f0413c2ec7256c676aa047ea90
David Romaní <david@flux.cat>
2021-01-08 20:22:24 +0100

    add cookies consent behaviour - 3

--- assets/app.js
+++ assets/app.js
@@ -12,19 +12,16 @@ $(document).ready(function() {
     let cookiesConsentButtonDeclineAllNode = $('#cookies-consent-button-decline-all');
     let cookiesConsentButtonAcceptAllNode = $('#cookies-consent-button-accept-all');
     let customSwitch2Node = $('#customSwitch2');
-    console.log('cookieConsentName', cookieConsentName);
     const cookieConsentOptions = {
         name: cookieConsentName,
         value: 'yes',
         maxAge: 31536000, // 1 year
-        // expires: new Date(2099, 10, 1), // optional Date,
         path: '/',
         secure: false,
         sameSite: 'lax' // optional enum 'lax' | 'strict' | 'none'
     };
     cookiesConsentButtonDeclineAllNode.bind('click', 'button', function(event) {
         event.preventDefault();
-        console.log('cookies-consent-button-decline-all');
         setCookie(cookieConsentOptions);
         setCookie({
             name: cookieConsentName+'_GTM',
@@ -42,8 +39,7 @@ $(document).ready(function() {
     if (cookieExists(cookieConsentName+'_GTM') && cookieHasValue(cookieConsentName+'_GTM', 'no')) {
         customSwitch2Node.prop('checked', false);
     }
-    customSwitch2Node.bind('click', 'input', function(event) {
-        console.log('customSwitch2Node.bind', customSwitch2Node.prop('checked'));
+    customSwitch2Node.bind('click', 'input', function() {
         if (customSwitch2Node.prop('checked')) {
             cookiesConsentButtonDeclineAllNode.prop('disabled', true);
             cookiesConsentButtonAcceptAllNode.prop('disabled', false);
@@ -54,7 +50,6 @@ $(document).ready(function() {
     });
     cookiesConsentButtonAcceptAllNode.bind('click', 'button', function(event) {
         event.preventDefault();
-        console.log('cookies-consent-button-accept-all');
         setCookie(cookieConsentOptions);
         setCookie({
             name: cookieConsentName+'_GTM',
@@ -70,18 +65,7 @@ $(document).ready(function() {
         if (!cookieExists(cookieConsentName)) {
             $('#staticBackdropModal').modal('show');
         }
-
-        // let _gdpr = _gdpr || [];
-        // _gdpr.push([
-        //     {
-        //         name: 'Google Tag',
-        //         type: 'stats',
-        //         description: 'Service pour statistiques des visites'
-        //     },
-        //     [
-        //         function(helpers) {
-        //             console.log('Helpers!', helpers);
-                    // // use helpers
+        // TODO add GTM script
                     // helpers.createScript('https://www.googletagmanager.com/gtag/js?id=' + keys_api.gtag);
                     //
                     // window.dataLayer = window.dataLayer || [];
@@ -90,11 +74,6 @@ $(document).ready(function() {
                     // }
                     // gtag('js', new Date());
                     // gtag('config', '########');
-        //         }
-        //     ]
-        // ]);
-        // initGdprCookie(locale);
-        // window._gdpr_showModal();
     }
     $('[data-toggle="popover"]').popover();
     $('#navbarSupportedContent').bind('click', 'ul li a', function(event) {
